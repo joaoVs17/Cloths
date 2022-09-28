dd99ff0304f67331425ef68bd3e83dc758a1dc25
João Vitor Simão <joaosimao051@gmail.com>
2022-09-28 11:02:52 -0300

    Sistema de Cadastro Aprimorado. Ainda tem Erros. Ele não identifica cpfs já registrados

--- user/views.py
+++ user/views.py
@@ -5,13 +5,28 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-
-
+# Exceções
+class ErrorEmail(Exception):
+    pass
+class ErrorPassword(Exception):
+    pass
+class EmailAlreadyExists(Exception):
+    pass
+class CpfAlreadyExists(Exception):
+    pass
+class RgAlreadyExists(Exception):
+    pass
+class TelefoneAlreadyExistes(Exception):
+    pass
+#views
 
 class Cadastro(View):
     def get(self, request):
-        return render(request, 'creatAcount.html')
+        status = request.GET.get('status')
+        context = {
+            'status': status,
+        }
+        return render(request, 'creatAcount.html', context)
     def post(self, request):
         nome = request.POST.get('nome')
         cpf = request.POST.get('cpf')
@@ -28,19 +43,48 @@ class Cadastro(View):
         senhaConfirmar = request.POST.get('senhaConfirmar')
         emailConfirmar = request.POST.get('emailConfirmar')
 
-        if senha and senhaConfirmar and email and emailConfirmar and senha==senhaConfirmar and email==emailConfirmar:
-            try:
-                db = get_user_model()
-                db.objects.create_user(nome=nome, cpf=cpf, rg=rg, telefone=telefone, email=email, password=senha, cep=cep, estado=estado,
-                cidade=cidade, rua=rua, bairro=bairro)
-                user = authenticate(cpf=cpf, email=email, telefone=telefone)
-                login(request, user)
-                return redirect('/?status=0')
-            except Exception:
-                return redirect('/?status=1')#status=1 é um erro do sistema
-        else:
-            return redirect('/usuario/cadastro/?status=2') #status=2 é a não correspondência do email e/ou da senha
+        try:
+            db = get_user_model()
 
+            #aqui são verificados os possíveis erros
+            print(db.objects.filter(cpf=cpf))
+            if senha != senhaConfirmar:
+                raise ErrorPassword
+            if email != emailConfirmar:
+                raise ErrorEmail
+            if len(db.objects.filter(cpf=cpf)>0):
+                raise CpfAlreadyExists
+            if len(db.objects.filter(rg=rg)>0):
+                raise RgAlreadyExists
+            if len(db.objects.filter(telefone=telefone)>0):
+                raise TelefoneAlreadyExistes
+            if len(db.objects.filter(email=email)>0):
+                raise EmailAlreadyExists
+            #se nenhum erro rolou, é criado o usuário
+
+            db.objects.create_user(nome=nome, cpf=cpf, rg=rg, telefone=telefone, email=email, password=senha, cep=cep, estado=estado,
+            cidade=cidade, rua=rua, bairro=bairro)
+            user = authenticate(request, email=email, password=senha)
+            login(request, user)
+            return redirect('/?status=0')
+
+            #aqui estão as ações conforme o erro
+        except CpfAlreadyExists:
+            return redirect('/usuario/cadastro/?status=1')
+        except RgAlreadyExists:
+            return redirect('/usuario/cadastro/?status=2')
+        except TelefoneAlreadyExistes:
+            return redirect('/usuario/cadastro/?status=3')
+        except ErrorEmail:
+            return redirect('/usuario/cadastro/?status=4')    
+        except ErrorPassword:
+            return redirect('/usuario/cadastro/?status=5')
+        except EmailAlreadyExists:
+            return redirect('/usuario/cadastro/?status=6')
+        except Exception:
+            return redirect('/usuario/cadastro/?status=9')#status=3 erro do sistema)
+        
+        
 
 class Login(View):
     def get(self, request):
@@ -51,8 +95,6 @@ class Login(View):
 
         if request.user.is_authenticated == False: #basicamente, isso só deixa o usuário logar se ele estiver deslogado antes
             user = authenticate(request, email=email, password=senha)
-            print(user)
-            print('batata')
             login(request, user)
             return redirect('home')
         elif request.user.is_authenticated == True:
